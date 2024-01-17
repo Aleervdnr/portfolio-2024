ccc7fc7baa6bf06a1c06f47bc93afbcfd42396da
Alejo <alejonahuelrott@gmail.com>
2024-01-17 12:18:46 -0300

    change name me image

--- src/componentes/AboutMe.jsx
+++ src/componentes/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <div className="bg-fondo-negro grid place-items-center py-8 md:grid-cols-2 px-6 lg:px-28" id="sobremi">
 
-            <div className=" relative w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] bg-anaranjado before:absolute before:w-[250px] before:h-[250px] lg:before:w-[350px] lg:before:h-[350px] before:bg-fondo-negro2 before:translate-y-[8px] before:translate-x-[8px] after:absolute after:w-[250px] after:h-[250px] lg:after:w-[350px] lg:after:h-[350px] after:bg-me after:bg-cover after:bg-bottom after:z-[10] z-[10] after:translate-y-[16px] after:translate-x-[16px] mb-[50px]"></div>
+            <div className=" relative w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] bg-anaranjado before:absolute before:w-[250px] before:h-[250px] lg:before:w-[350px] lg:before:h-[350px] before:bg-fondo-negro2 before:translate-y-[8px] before:translate-x-[8px] after:absolute after:w-[250px] after:h-[250px] lg:after:w-[350px] lg:after:h-[350px] after:bg-[url('./src/assets/me.jpg')] after:bg-cover after:bg-bottom after:z-[10] z-[10] after:translate-y-[16px] after:translate-x-[16px] mb-[50px]"></div>
 
             <div className="max-w-[500px] lg:max-w-[520px] text-center text-white font-light text-[14px] xl:text-base leading-4">
                 <p className="mb-2 ">
